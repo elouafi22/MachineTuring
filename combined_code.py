6099d6baa9fe07c2efd3d96a03169f65e2bf809b
Abdellah EL BOUAZZAOUI <etudiant.el.bouazzaoui.abdellah@gmail.com>
2023-04-01 16:46:29 +0000

    Additional changes

--- combined_code.py
+++ combined_code.py
@@ -22,24 +22,23 @@ class Automate:
             print(self.etatFinals)
             print(self.transitions)
             '''
-            
-            
+                      
 
     def getTransitions(self):
         "cette methode return la table de trasition sous la forme d'un dictionaire"
         return self.transitions
 
     def getInformations(self):
-        "cette methode return le nom du transitionsramme a faire plus l'etat initiale et l'etat finale et le mode d'utilisation de la machine(accepteur/calculateur) sou la forme d'une liste "
+        "cette methode return le nom du transitionsramme a faire plus l'etat initiale et l'etat finale et le mode d'utilisation de la machine(reconnaisseur/calculateur) sou la forme d'une liste "
         return [self.titre, self.etatInitial, self.etatFinals, self.mode]
 
 
 class MachineDeTuring:
-    def __init__(self, fichier, probleme, N=100):
-        "initialisation de la machine avec le probleme et le transitionsrmme"
+    def __init__(self, fichier, instance, N=100):
+        "initialisation de la machine avec le programme et l'instance'"
         self.tete = N//2
-        self.probleme = probleme
-        self.ruban = ''.join('#'*self.tete)+self.probleme+''.join('#'*self.tete)
+        self.instance = instance
+        self.ruban = ''.join('#'*self.tete)+self.instance+''.join('#'*self.tete)
         self.automate = Automate(fichier)
         self.titre = self.automate.getInformations()[0]
         self.transitions = self.automate.getTransitions()
@@ -48,7 +47,7 @@ class MachineDeTuring:
         self.mode = self.automate.mode
 
 
-    def afficherInstruction(self, i):
+    def afficherIteration(self, i):
         "cette methode permet d'afficher une itération de la machine"
         print(self.ruban)
         print(''.join(' '*i)+'^')
@@ -56,36 +55,40 @@ class MachineDeTuring:
 
 
     def execution(self):
-        "execution du transitionsramme sur la machine "
+        "execution du programme modèlisé par la machine"
         i = self.tete
         self.etatCourant = self.etatInitial
         while self.etatCourant not in self.etatFinals:
-            if self.transitions.__contains__((self.etatCourant, self.ruban[i])) == True:
+            transition = self.transitions.get((self.etatCourant, self.ruban[i]),None)
+            if transition:
                 self.ruban = list(self.ruban)
-                val1 = self.ruban[i]
-                self.ruban[i] = self.transitions[(self.etatCourant, self.ruban[i])][1]
+                self.ruban[i] = transition[1]
+                
                 self.ruban = ''.join(self.ruban)
-                self.afficherInstruction(i)
-                if(self.transitions[(self.etatCourant, val1)][2] == 'R'):
+                self.afficherIteration(i)
+
+                if(transition[2] == 'R'):
                     i = i+1
-                elif(self.transitions[(self.etatCourant, val1)][2] == 'L'):
+                elif(transition[2] == 'L'):
                     i = i-1
-                self.etatCourant = self.transitions[(self.etatCourant, val1)][0]
+                
+                self.etatCourant = transition[0]
                 print("etat suivant : "+self.etatCourant)
-                print("-------------------------------------------")
+                print(20*'-')
                 time.sleep(1)
 
             else:
-                if(self.mode == "accepteur"):
-                    print("le mot n'est pas roconnue")
+                if(self.mode == "reconnaisseur"):
+                    print("le mot n'est pas reconnu")
                 break
-        if(self.etatCourant in self.etatFinals and self.mode == "accepteur"):
-            print("le mot est rocconue")
+        if(self.etatCourant in self.etatFinals and self.mode == "reconnaisseur"):
+            print("le mot est reconnu")
         
     def console(self):
-        print('------------------------- ' + self.titre + '------------')
+        print(10*'-' + self.titre + 10*'-')
         print()
         self.execution()
 
-#Automate("aNbN.txt")
-MachineDeTuring("aNbNcN.txt","aabbcc").console()
+
+MachineDeTuring("aNbNcN.txt","bbcc").console()
+
